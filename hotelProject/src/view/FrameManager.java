6f36f909e46887451a47a81099dc4d468d189581
SIMON <605817494@qq.com>
2019-12-21 12:56:56 +0800

    1

--- hotelProject/src/view/FrameManager.java
+++ hotelProject/src/view/FrameManager.java
@@ -11,7 +11,7 @@ public class FrameManager extends javax.swing.JFrame {
      */
     public FrameManager() {
         initComponents(); 
-        this.setVisible(true);
+//        this.setVisible(true);
     }
     
     
